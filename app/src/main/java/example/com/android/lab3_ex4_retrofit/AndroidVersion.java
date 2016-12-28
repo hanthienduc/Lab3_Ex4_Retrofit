1d3f814e2c96821a86e62e8f295d79f3b6622c37
Han Thien Duc <hanthienduc.96@gmail.com>
2016-12-28 10:19:45 +0700

    Initial commit

---
+++ app/src/main/java/example/com/android/lab3_ex4_retrofit/AndroidVersion.java
@@ -0,0 +1,23 @@
+package example.com.android.lab3_ex4_retrofit;
+
+/**
+ * Created by Han on 27/12/2016.
+ */
+
+public class AndroidVersion {
+    private String ver;
+    private String name;
+    private String api;
+
+    public String getVer() {
+        return ver;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getApi() {
+        return api;
+    }
+}
---
+++ app/src/main/java/example/com/android/lab3_ex4_retrofit/JSONResponse.java
@@ -0,0 +1,13 @@
+package example.com.android.lab3_ex4_retrofit;
+
+/**
+ * Created by Han on 27/12/2016.
+ */
+
+public class JSONResponse {
+    private AndroidVersion[] android;
+
+    public AndroidVersion[] getAndroid() {
+        return android;
+    }
+}
